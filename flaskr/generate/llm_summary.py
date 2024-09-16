2102265a8cc0b5387205ca889711f63634b5f294
NaDub <n.dubourg450@gmail.com>
2024-09-16 17:14:17 +0200

    modif llm_summary, on peut maintenant créer plein de fonctions qui utilisent la meme logique de l API en changeant juste le prompt

--- flaskr/generate/llm_summary.py
+++ flaskr/generate/llm_summary.py
@@ -1,7 +1,9 @@
-import os
 import re
+import os
 import time
-
+import replicate
+from replicate.exceptions import ReplicateError
+from functools import wraps
 response_file = "explanation.txt"
 import replicate
 from replicate.exceptions import ReplicateError
@@ -18,13 +20,45 @@ else:
     replicate.api_token = REPLICATE_API_TOKEN
 
 
-def synthesize_infos_with_llm(text: str) -> str | None:
-    """Envoie le texte à Llama3 via l'API Replicate pour synthétiser les infos twitter."""
+def api_request_decorator(objective: str, instructions: str):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(text: str, *args, **kwargs):
+            input_data = {
+                'prompt': (
+                    f"**Objective:** {objective}\n\n"
+                    f"{instructions}\n\n"
+                    f"**Original Prompt:**\n{text}"
+                )
+            }
 
-    prompt_objective = (
-        '**Objective:** Synthetise les infos sur la sortie du scan One Piece.'
-    )
-    prompt_instructions = (
+            print(f"Infos envoyées à Llama3: {text}")
+
+            try:
+                output = replicate.run(
+                    "meta/meta-llama-3-70b-instruct", input=input_data)
+                explanations = "".join(output)  # Joindre les parties de la réponse
+                return explanations
+
+            except ReplicateError as e:
+                print(f"Erreur liée à Replicate lors de l'envoi de la demande: {e}")
+                time.sleep(10)
+            except FileNotFoundError as e:
+                print(f"Erreur : Le fichier spécifié n'a pas été trouvé: {e}")
+                time.sleep(10)
+            except OSError as e:
+                print(f"Erreur système : {e}")
+                time.sleep(10)
+            except ValueError as e:
+                print(f"Erreur de valeur : {e}")
+                time.sleep(10)
+            return None
+        return wrapper
+    return decorator
+
+@api_request_decorator(
+    objective="Synthetise les infos sur la sortie du scan One Piece.",
+    instructions=(
         '- **Synthesize the informations**'
         ' Below you will find the reasons why the last One Piece scan is not available.\n'
         ' Using theses informations you will write a paragraph to explain the situation to one piece fans.\n'
@@ -33,37 +67,23 @@ def synthesize_infos_with_llm(text: str) -> str | None:
         ' You will only write the explanations in quote and do not write any introduction phrase before.\n'
         ' Your answers should always contain close to 500 characters.'
     )
+)
+def synthesize_infos_with_llm(text: str) -> str | None:
+    """Envoie le texte à Llama3 via l'API Replicate pour synthétiser les infos Twitter."""
+    return text  
 
-    input_data = {
-        'prompt': (
-            f"{prompt_objective}\n\n"
-            f"{prompt_instructions}\n\n"
-            f"**Original Prompt:**\n{text}"
-        )
-    }
-
-    print(f"Infos envoyées à Llama3: {text}")
-
-    try:
-        output = replicate.run(
-            "meta/meta-llama-3-70b-instruct", input=input_data)
-        explanations = "".join(output)  # Joindre les parties de la réponse
-        return explanations
-
-    except ReplicateError as e:
-        print(f"Erreur liée à Replicate lors de l'envoi de la demande: {e}")
-        time.sleep(10)
-    except FileNotFoundError as e:
-        print(f"Erreur : Le fichier spécifié n'a pas été trouvé: {e}")
-        time.sleep(10)
-    except OSError as e:
-        print(f"Erreur système : {e}")
-        time.sleep(10)
-    except ValueError as e:
-        print(f"Erreur de valeur : {e}")
-        time.sleep(10)
-    return None
-
+@api_request_decorator(
+    objective="Synthetise les infos sur la sortie du scan One Piece.",
+    instructions=(
+        '- **Synthesize the informations**'
+        ' Below you will find a one piece text of a video.\n'
+        ' Using theses informations you will write a paragraph to explain the situation to one piece fans.\n'
+        ' You wil write a paragraph that clearly summarize the video\n'
+        ' Your answers should always contain close to 500 characters.'
+    )
+)
+def synthesize_video_with_llm(text: str) -> str | None:
+    return text  
 
 def replace_body_content(new_content: str, file_path: str = "explanation.html"):
     try:
