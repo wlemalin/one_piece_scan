88b39a0d5163845d7edb74252ac9eeb0bfc4d0cb
wlemalin <124249669+wlemalin@users.noreply.github.com>
2025-01-12 15:58:14 +0100

    Update actu_x_scraping.py

--- flaskr/scraping/actu_x_scraping.py
+++ flaskr/scraping/actu_x_scraping.py
@@ -2,31 +2,29 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def fetch_actus_tweets(url='https://x.com/OnePieceAnime', output_file='actus_twitter.txt', max_tweets=30):
-
+def fetch_actus_tweets(url='https://x.com/OnePieceAnime', max_tweets=30):
     # Initialiser le navigateur
     driver = webdriver.Chrome()
+    text_output = ""
 
     try:
         driver.get(url)
         time.sleep(10)  # Temps pour charger la page
 
-        # Ouvrir un fichier en mode écriture
-        with open(output_file, 'w', encoding='utf-8') as file:
-            tweets = driver.find_elements(By.XPATH, '//*[@data-testid="tweetText"]')
-            count = 0
+        tweets = driver.find_elements(By.XPATH, '//*[@data-testid="tweetText"]')
+        count = 0
 
-            for tweet in tweets:
-                if count >= max_tweets:
-                    break
+        for tweet in tweets:
+            if count >= max_tweets:
+                break
 
-                tweet_text = tweet.text
-                file.write(f"Tweet {count + 1} :\n")
-                file.write(tweet_text + '\n')
-                file.write('-' * 50 + '\n')
-                count += 1
+            tweet_text = tweet.text
+            text_output += f"Tweet {count + 1} :\n"
+            text_output += tweet_text + '\n'
+            text_output += '-' * 50 + '\n'
+            count += 1
 
-            print(f"{count} tweets ont été enregistrés dans le fichier '{output_file}'.")
+        print(f"{count} tweets ont été récupérés.")
 
     except Exception as e:
         print("Erreur lors de la récupération des tweets :", e)
@@ -34,3 +32,5 @@ def fetch_actus_tweets(url='https://x.com/OnePieceAnime', output_file='actus_twi
     finally:
         # Fermer le navigateur
         driver.quit()
+
+    return text_output
